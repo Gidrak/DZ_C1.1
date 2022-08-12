ad2d0eedfe26e224896e1a3255d0cab9d0602c81
Gidrak <paskark@gmail.com>
2022-08-12 13:46:39 +0300

    Добавил игнор и нужные команды

---
+++ d_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+int a, b;
+// Начало Программыю
+Console.WriteLine("Введите два целых числа для сравнения: ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("{0} наибольшее число", a);
+    Console.WriteLine("{0} наименьшее число", b);
+}
+else
+{
+    Console.WriteLine("{0} наибольшее число ", b);
+    Console.WriteLine("{0} наименьшее число", a);
+}
---
+++ d_z4/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+int a, b, c, max;
+//начало программы
+Console.WriteLine("Введите три целых числа для сравнения: ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    max = a;
+ }
+else
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine("{0} наибольшее число", max);
